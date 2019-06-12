e6cfee8790cae6c660190af31c411286abc8b765
zhuli <745381696@qq.com>
2019-06-12 16:53:27 +0800

    333

---
+++ git/src/cn/itheima/trst.java
@@ -0,0 +1,12 @@
+package cn.itheima;
+
+public class trst {
+    public static void main(String[] args) {
+        System.out.println("wwwwww");
+        System.out.println("wwwwww");
+        System.out.println("wwwwww");
+        System.out.println("wwwwww");
+        System.out.println("wwwwww");
+        System.out.println("wwwwww");
+    }
+}
